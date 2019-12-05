4bb6777aab8cf9ca15dfea9fc558473aafb7d972
@xanderlav <alejandroregino.hussmann.com>
2019-12-05 08:51:23 -0600

    Server & friends javascript files update

---
+++ app/data/friend.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+      name: "Eric Cartman",
+      photo: "https://st-listas.20minutos.es/images/2013-02/354750/3906593_640px.jpg?1539287571",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Kenny McCormick",
+      photo: "https://st-listas.20minutos.es/images/2013-02/354750/3906454_640px.jpg?1539287571",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Kyle Broflovski",
+      photo: "https://st-listas.20minutos.es/images/2013-02/354750/3906592_640px.jpg?1539287571",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Butters Stotch",
+      photo: "https://st-listas.20minutos.es/images/2013-02/354750/3906590_640px.jpg?1539287571",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Stan Marsh",
+      photo: "https://st-listas.20minutos.es/images/2013-02/354750/3906591_640px.jpg?1539287571",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Tweek Tweak",
+      photo: "https://st-listas.20minutos.es/images/2013-02/354750/3906594_640px.jpg?1539287571",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // export friend-finder array
+  module.exports = friends;
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-// Declaring variables
+// Declaring global variables
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 
+var app = express();
 
+var PORT = process.env.PORT || 7000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use('/static', express.static(path.join(__dirname, 'app/public')))
+
+// ================================================================================
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+// The below code effectively "starts" our server
+app.listen(PORT, function() {
+  console.log(`Friend-Finder App listening on PORT: ${PORT}`);
+});
